61e20b16f580f992b4afd52067679c7196f0da0d
John Long <johnlong@amcsgroup.com>
2020-08-06 16:17:03 +0100

    initial commit

---
+++ http-01-start/src/app/posts.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Post } from './post.model';
+
+import { map, catchError } from 'rxjs/operators'
+import { Subject, throwError } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class PostsService {
+    error = new Subject<string>();
+    
+    constructor(private http: HttpClient) {}
+
+    createAndStorePost(title: string, content: string) {
+        const postData: Post = {title: title, content: content}
+        this.http
+        .post<{ name:string }>(
+          'https://angular-course-project-834ca.firebaseio.com/posts.json',
+          postData
+        )
+        .subscribe(responseData => {
+          console.log(responseData);
+        }, error => {
+            this.error.next(error.message);
+        });
+    }
+
+    fetchPosts() {
+        return this.http
+        .get<{ [key: string]: Post }>(
+            'https://angular-course-project-834ca.firebaseio.com/posts.json'
+        )
+        .pipe(
+          map(responseData => {
+            const postsArray: Post[] = [];
+            for(const key in responseData) {
+              if(responseData.hasOwnProperty(key)){
+                postsArray.push({...responseData[key], id: key});
+              }
+            }
+            return postsArray;
+          }),
+          catchError(errorRes => {
+              // some generic error handling task goes here
+            return   throwError(errorRes);
+          })
+        );
+    }
+
+    deletePosts() {
+      return this.http.delete('https://angular-course-project-834ca.firebaseio.com/posts.json')
+    }
+    
+}
